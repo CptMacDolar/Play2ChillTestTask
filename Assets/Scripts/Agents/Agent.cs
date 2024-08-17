feb2d18ea64abd276437d5d2407cfa478f92c1e8
MacDolar <macdolar167@gmail.com>
2024-08-17 16:34:20 +0200

    Agent Movement

--- Assets/Scripts/Agents/Agent.cs
+++ Assets/Scripts/Agents/Agent.cs
@@ -1,20 +1,184 @@
 using System;
+using System.Threading.Tasks;
+using Core;
+using DG.Tweening;
+using Pathfinding;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Agents
 {
     public class Agent : MonoBehaviour
     {
-        private Guid _guid;
+        [Header("Parameters")]
+        [SerializeField] private float nextWaypointDistance = 0.25f;
+        [SerializeField] private float moveRange = 15f;
+        [SerializeField] private float baseMoveSpeed = 5f;
+        [SerializeField] private float rotationDuration = 0.5f;
         
-        public void OnSpawn(float moveRange)
+        private Guid _guid;
+        private float _gameSpeed = 1f;
+        
+        // Flags
+        private bool _canMove;
+        private bool _isMoving;
+        private bool _paused;
+        
+        //DOTween
+        private Tween _currentTween;
+        
+        // A* Path Finding
+        private Seeker _seeker;
+        private Path _path;
+        private int _currentWaypoint;
+        private Vector3 _destination;
+        
+        
+        private void Awake()
+        {
+            _seeker = GetComponent<Seeker>();
+            _seeker.pathCallback += OnPathComplete;
+
+        }
+        
+        public void OnDisable ()
+        {
+            _seeker.pathCallback -= OnPathComplete;
+        }
+        
+        private void OnPathComplete(Path path)
+        {
+            if (path.error)
+            {
+                Debug.LogError(path.errorLog);
+                return;
+            }
+
+            _path = path;
+            _currentWaypoint = 0;
+
+            if (!_isMoving)
+                MoveAgentAsync();
+        }
+        
+        public void OnSpawn()
         {
             _guid = Guid.NewGuid();
+            _canMove = true;
+            
+            SetRandomDestination();
         }
         
         public void OnTick()
         {
-            throw new System.NotImplementedException();
+            if(_paused)
+                return;
+            
+            if (_path == null)
+                return;
+
+            while (true)
+            {
+                if(_paused)
+                    return;
+                
+                var distanceToWaypoint = Vector3.Distance(transform.position, _path.vectorPath[_currentWaypoint]);
+                if (distanceToWaypoint < nextWaypointDistance)
+                {
+                    if (_currentWaypoint + 1 < _path.vectorPath.Count)
+                    {
+                        _currentWaypoint++;
+                        ResetMovement();
+                    }
+                    else
+                    {
+                        OnMovementComplete();
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        
+        private void SetRandomDestination()
+        {
+            var randomPosition = Random.insideUnitSphere * moveRange;
+            randomPosition.y = transform.position.y;
+
+            _destination = randomPosition;
+            _seeker.StartPath(transform.position, _destination);
+        }
+        
+        private async void MoveAgentAsync()
+        {
+            while (_canMove)
+            {
+                if (!_isMoving)
+                {
+                    ResetMovement();
+                }
+                await Task.Yield();
+            }
+        }
+        
+        public void ChangeGameSpeed(float gameSpeed)
+        {
+            if (_paused)
+                ChangePaused(false);
+            
+            _gameSpeed = gameSpeed;
+            
+            if(_isMoving)
+                ResetMovement();
+        }
+        
+        private void ResetMovement()
+        {
+            if(_paused)
+                return;
+            
+            var currentDestination = _path.vectorPath[_currentWaypoint];
+            var currentSpeed = baseMoveSpeed * _gameSpeed;
+            float distance = Vector3.Distance(transform.position, currentDestination);
+            float duration = distance / currentSpeed;
+            
+            _currentTween?.Kill();
+            _isMoving = true;
+
+            transform.DOLookAt(currentDestination, rotationDuration / _gameSpeed);
+
+            _currentTween = transform.DOMove(currentDestination, duration)
+                .SetEase(Ease.Linear);
+        }
+
+        public void ChangePaused(bool paused)
+        {
+            _paused = paused;
+            
+            if(_currentTween == null)
+                return;
+            
+            if (paused)
+            {
+                if(_currentTween.IsPlaying())
+                    _currentTween.Pause();
+            }
+            else
+            {
+                if(!_currentTween.IsPlaying())
+                    _currentTween.Play();
+            }
+        }
+        
+        private void OnMovementComplete()
+        {
+            var log = $"Agent {_guid} arrived";
+            GameManager.Instance.AgentService.LogAgent(log);
+            SetRandomDestination();
+            _isMoving = false;
         }
     }
 }
